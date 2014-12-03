c9e95a63c6a018bb3d8e8090699e5882fb4ec024
atoder <alexandert@gmail.com>
2014-12-02 19:01:27 -0800

    Homework 3 Cucumber

---
+++ hw3/bdd-cucumber/rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -0,0 +1,42 @@
+# Add a declarative step here for populating the DB with movies.
+
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
+  end
+end
+
+# Make sure that one string (regexp) occurs before or after another one
+#   on the same page
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  assert page.body =~ /#{e1}.*#{e2}/m, "#{e1} was not before #{e2}"
+end
+
+# Make it easier to express checking or unchecking several boxes at once
+#  "When I uncheck the following ratings: PG, G, R"
+#  "When I check the following ratings: G"
+
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  # HINT: use String#split to split up the rating_list, then
+  #   iterate over the ratings and reuse the "When I check..." or
+  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(',').each do |rating|
+      if uncheck
+        uncheck "ratings_#{rating}"
+      else
+        check "ratings_#{rating}"
+      end
+    end
+end
+
+Then /I should see all of the movies/ do
+  movies = Movie.all
+  movies.each do |movie|
+    assert page.body =~ /#{movie.title}/m, "#{movie.title} did not appear"
+  end
+end
